3ff95b177c3a5eade09e63b6b479e91967839ce6
marian122 <maleksiev12@gmail.com>
2020-10-18 18:00:58 +0300

    added mdbootstrap, configured some things

--- Web/ODZ.Web/ClientApp/src/app/app.module.ts
+++ Web/ODZ.Web/ClientApp/src/app/app.module.ts
@@ -6,31 +6,31 @@ import { RouterModule } from '@angular/router';
 import { MDBBootstrapModule  } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    FooterComponent
+    FooterComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MDBBootstrapModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
-    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
