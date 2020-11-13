897cdb8d408cbf112ff3a64d41f759d04d9b7c53
marian122 <maleksiev12@gmail.com>
2020-11-13 18:11:16 +0200

    FINAL PUSH FOR TODAY

--- Services/ODZ.Services/DocumentService.cs
+++ Services/ODZ.Services/DocumentService.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ODZ.Mappings;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace ODZ.Services
---
+++ Web/ODZ.Web/ViewModels/AllDocumentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODZ.Web.ViewModels
+{
+    public class AllDocumentsViewModel
+    {
+        public IEnumerable<DocumentViewModel> AllDocuments { get; set; }
+    }
+}
