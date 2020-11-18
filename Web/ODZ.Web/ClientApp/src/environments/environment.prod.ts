ad3381071583a4b3fadb074adfd528da18e3cb74
marian122 <maleksiev12@gmail.com>
2020-11-18 14:14:42 +0200

    trying to deployy

--- Web/ODZ.Web/ClientApp/src/environments/environment.prod.ts
+++ Web/ODZ.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
   apiUrl: 'https://localhost:44345'
-=======
-  apiUrl: 'https://localhost:44338/'
->>>>>>> 4c6efad61993b7bb16cc578de5d5aa752a891972
 };
